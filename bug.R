a161e7385a489998f0d67267bec62d5b2fee983b
BugHunter-X <askzenai@gmail.com>
2025-02-24 15:10:06 +0100

    Add bug.R file

---
+++ bug.R
@@ -0,0 +1,9 @@
+```R
+# This code attempts to read a CSV file, but it will fail if the file doesn't exist.
+
+file_path <- "my_data.csv"
+data <- read.csv(file_path)
+
+# Perform analysis on the data
+# ...
+```
